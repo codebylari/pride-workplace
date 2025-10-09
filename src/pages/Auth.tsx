5cb887b672ac1efdb382b8923765f93e3cb66729
codebylari <larissa.soeiroq@gmail.com>
2025-10-09 20:28:23 -0300

    att

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -4,7 +4,7 @@ import { supabase } from "@/integrations/supabase/client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useToast } from "@/hooks/use-toast";
-import logo from "@/assets/logo.png";
+import logo from "@/assets/Logo.png";
 
 export default function Auth() {
   const [email, setEmail] = useState("");
